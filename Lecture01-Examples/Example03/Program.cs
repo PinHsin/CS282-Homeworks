2b0ae2a650d19f75ca16034346394e180d6aad8c
PinHsin <regen_1010@hotmail.com>
2017-05-26 00:21:50 +0800

    新增類別範例

---
+++ Lecture01-Examples/Example03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student someone = new Student();
+            someone.name = "侯品欣";
+            Console.WriteLine("學生姓名:{0}", someone.name);
+        
+           
+        }
+    }
+    }
+
